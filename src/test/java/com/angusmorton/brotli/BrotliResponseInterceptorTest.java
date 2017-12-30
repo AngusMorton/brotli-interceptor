0ad6ec067573e47c2559e6359df6fa5bc1e6f4f3
Angus Morton <angus.m.morton@gmail.com>
2017-12-30 15:25:44 +1100

    Fix Tests

--- src/test/java/com/angusmorton/brotli/BrotliResponseInterceptorTest.java
+++ src/test/java/com/angusmorton/brotli/BrotliResponseInterceptorTest.java
@@ -1,9 +1,15 @@
 package com.angusmorton.brotli;
 
 import okhttp3.*;
+import okhttp3.internal.http.RealResponseBody;
+import okhttp3.internal.tls.SslClient;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okio.Buffer;
+import okio.ByteString;
+import okio.Okio;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -13,31 +19,37 @@ import static com.angusmorton.brotli.TestUtil.resource;
 import static org.junit.Assert.*;
 
 public final class BrotliResponseInterceptorTest {
-  private static final ConnectionPool connectionPool = new ConnectionPool();
-  private static final Dispatcher dispatcher = new Dispatcher();
-
-  public static OkHttpClient defaultClient() {
-    return new OkHttpClient.Builder()
-        .connectionPool(connectionPool)
-        .dispatcher(dispatcher)
-        .dns(new SingleInetAddressDns()) // Prevent unexpected fallback addresses.
-        .build();
-  }
 
   @Rule
   public MockWebServer server = new MockWebServer();
 
-  private OkHttpClient client = defaultClient();
+  private SslClient sslClient = SslClient.localhost();
+  private OkHttpClient client;
+  private String host;
+  private HttpUrl url;
+
+  @Before
+  public void setUp() {
+    client = new OkHttpClient.Builder()
+        .addNetworkInterceptor(new BrotliResponseInterceptor())
+        .sslSocketFactory(sslClient.socketFactory, sslClient.trustManager)
+        .build();
+
+    host = server.getHostName() + ":" + server.getPort();
+    url = server.url("/");
+  }
 
   @Test
-  public void applicationInterceptorsCanShortCircuitResponses() throws Exception {
-    server.enqueue(new MockResponse()
-        .setBody(resource("100kilobytes.br").buffer())
-        .addHeader("Content-Encoding: br"));
+  public void interceptorDecodeEncodedResponse() throws Exception {
+    Buffer brotliSource = new Buffer()
+        .write(ByteString.decodeHex("0b018061626303")); // 'abc'
+    MockResponse mockResponse = new MockResponse()
+        .setBody(brotliSource)
+        .addHeader("Content-Encoding: br");
 
-    Request request = new Request.Builder()
-        .url(server.url("/"))
-        .build();
+    server.enqueue(mockResponse);
+
+    Request request = request().build();
 
     client = client.newBuilder()
         .addNetworkInterceptor(new BrotliResponseInterceptor())
@@ -52,7 +64,10 @@ public final class BrotliResponseInterceptorTest {
     Assert.assertNull(response.header("Content-Encoding"));
 
     String actualResponse = response.body().string();
-    String expectedResponse = resource("100kilobytes.txt").readUtf8();
-    assertEquals(expectedResponse, actualResponse);
+    assertEquals("abc", actualResponse);
+  }
+
+  private Request.Builder request() {
+    return new Request.Builder().url(url);
   }
 }
--- src/test/java/com/angusmorton/brotli/internal/BrotliSourceTest.java
+++ src/test/java/com/angusmorton/brotli/internal/BrotliSourceTest.java
@@ -1,15 +1,12 @@
 package com.angusmorton.brotli.internal;
 
-import com.angusmorton.brotli.TestUtil;
-import okio.Buffer;
-import okio.Okio;
-import okio.Source;
+import okio.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static com.angusmorton.brotli.TestUtil.resource;
 import static org.junit.Assert.*;
@@ -18,32 +15,40 @@ public class BrotliSourceTest {
 
   @Test
   public void decode() throws IOException {
+    InputStream inputStream = new ByteArrayInputStream(ByteString.decodeHex("8b0f8049742773206120554e49582073797374656d212049206b6e6f7720746869732103").toByteArray());
+    Source brotliSource = BrotliSource.create(Okio.buffer(Okio.source(inputStream)));
 
-    Source brotliSource = BrotliSource.create(resource("100kilobytes.br"));
-
-    // Decode!
-    Buffer brotliDecoded = new Buffer();
-    while (brotliSource.read(brotliDecoded, Integer.MAX_VALUE) != -1) {
-    }
-
-    // BrotliDecoded now contains all of the decoded
-    String decoded = brotliDecoded.readUtf8();
-    String expected = resource("100kilobytes.txt").readUtf8();
+    String decoded = Okio.buffer(brotliSource).readUtf8();
+    String expected = "It's a UNIX system! I know this!";
 
     Assert.assertEquals(expected, decoded);
+  }
+
+  @Test
+  public void decodeWithBuffer() throws IOException {
+    Buffer brotliBuffer = new Buffer()
+        .write(ByteString.decodeHex("8b0f8049742773206120554e49582073797374656d212049206b6e6f7720746869732103"));
+    Source brotliSource = BrotliSource.create(brotliBuffer);
+
+    String decoded = Okio.buffer(brotliSource).readUtf8();
+
+    Assert.assertEquals("It's a UNIX system! I know this!", decoded);
   }
 
   @Test
   public void decodeErrorWhenNotBrotliEncoded() throws IOException {
     try {
       Source brotliSource = BrotliSource.create(resource("100kilobytes.txt"));
-
-      // Decode!
-      Buffer brotliDecoded = new Buffer();
-      while (brotliSource.read(brotliDecoded, Integer.MAX_VALUE) != -1) {
-      }
+      Okio.buffer(brotliSource).readUtf8();
       fail();
     } catch (IOException expected) {
     }
+  }
+
+  @Test
+  public void brotliDecodesShortString() throws Exception {
+    Buffer brotliBuffer = new Buffer().write(ByteString.decodeHex("0b018061626303")); // abc
+    BufferedSource brotliSource = Okio.buffer(BrotliSource.create(brotliBuffer));
+    Assert.assertEquals("abc", brotliSource.readUtf8());
   }
 }
